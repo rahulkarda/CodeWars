ec855bae70e60d1ea385719fddfdbe02b1c285e4
Rahul <rahulkarda2002@gmail.com>
2022-05-25 10:19:13 +0530

    twice_as_old

---
+++ twice_as_old.js
@@ -0,0 +1,10 @@
+/*
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+*/
+
+
+//Answer
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - 2*sonYearsOld);
+  }
