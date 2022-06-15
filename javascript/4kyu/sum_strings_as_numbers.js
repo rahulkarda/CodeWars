8fb7840392885c5b23bc50d2031329b5019ad8d7
Rahul <rahulkarda2002@gmail.com>
2022-06-15 15:06:20 +0530

    Sum strings as numbers

---
+++ javascript/4kyu/sum_strings_as_numbers.js
@@ -0,0 +1,23 @@
+/*
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+*/
+
+//Answer
+
+function sumStrings(a, b) {
+    let res = '', c = 0;
+    a = a.split('');
+    b = b.split('');
+    while (a.length || b.length || c) {
+        c += ~~a.pop() + ~~b.pop();
+        res = c % 10 + res;
+        c = c > 9;
+    }
+    return res.replace(/^0+/, '');
+}
