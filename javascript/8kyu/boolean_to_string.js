9d5834095e45b6004c6a927575500f8d1499b2dc
Rahul <rahulkarda2002@gmail.com>
2022-07-14 07:05:30 +0530

    Boolean to String

---
+++ javascript/8kyu/boolean_to_string.js
@@ -0,0 +1,13 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+
+//Answer
+
+function booleanToString(b) {
+  return b ? "true" : "false";
+}
