4311a68ea0a8305f5601d34cfa971f5cdd7343f1
Rahul <rahulkarda2002@gmail.com>
2022-07-26 11:42:08 +0530

    Sort the odd

---
+++ javascript/6kyu/sort_the_odd.js
@@ -0,0 +1,18 @@
+/*
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+*/
+
+
+//Answer
+
+function sortArray(arr) {
+  const odds = arr.filter((x) => x % 2).sort((a, b) => a - b);
+  return arr.map((x) => (x % 2 ? odds.shift() : x));
+}
